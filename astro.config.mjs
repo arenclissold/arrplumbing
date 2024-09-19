6a18a2753b20c47022f5418348e9ded19079ff2e
Aren Clissold <arenclissold@gmail.com>
2024-09-19 22:10:01 +0900

    remove quote form

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,7 @@ import tailwind from "@astrojs/tailwind"
 import react from '@astrojs/react'
 
 // https://astro.build/config
-import cloudflare from "@astrojs/cloudflare"
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: 'server',
-  adapter: cloudflare()
+  output: 'static',
 })
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="astro/client" />
-interface ImportMetaEnv {
-  readonly PUBLIC_TURNSTILE_SITEKEY: string
-  readonly TURNSTILE_SECRET: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
